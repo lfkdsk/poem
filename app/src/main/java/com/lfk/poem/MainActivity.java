4a360154872928e75e128c5bbbf4192d13349c80
lfkdsk <lfk_dsk@hotmail.com>
2015-05-29 17:50:07 +0800

    分享功能 5.29
    添加了分享功能

--- app/src/main/java/com/lfk/poem/MainActivity.java
+++ app/src/main/java/com/lfk/poem/MainActivity.java
@@ -2,18 +2,25 @@ package com.lfk.poem;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.TypedArray;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.Rect;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.ShareActionProvider;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,6 +32,7 @@ import java.io.InputStream;
 
 
 public class MainActivity extends Activity {
+    private ShareActionProvider mShareActionProvider;
     private  final int BUFFER_SIZE = 400000;
     public static final String DB_NAME = "poem_all.db"; //保存的数据库文件名
     //public static final String DB_USER_NAME = "poem_user.db";
@@ -36,6 +44,8 @@ public class MainActivity extends Activity {
     private RelativeLayout main_layout;
     private TextView textView;
     private static int ID = 0;
+    private String poem;
+    private Intent sendIntent;
     //private String NAME;
     //private String POEM;
     @Override
@@ -50,6 +60,9 @@ public class MainActivity extends Activity {
         main_layout = (RelativeLayout)findViewById(R.id.main_layout);
         //ChangeBackground();
         FindaPoem();
+
+        //setmShareActionProvider();
+
         swipeLayout = (SwipeRefreshLayout) this.findViewById(R.id.swipe_refresh);
         swipeLayout.setColorScheme(R.color.haah);
         swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
@@ -71,7 +84,7 @@ public class MainActivity extends Activity {
         SQLiteDatabase database = openDatabase();
         Cursor cursor = database.rawQuery("Select * From poem Where _id = " + ll, null);
         cursor.moveToFirst();
-        String poem = cursor.getString(1)+"\n"+"\n"+cursor.getString(2)+"\n"+"\n"+cursor.getString(13);
+        poem = cursor.getString(1)+"\n"+"\n"+cursor.getString(2)+"\n"+"\n"+cursor.getString(13);
         //NAME = cursor.getString(2)+": "+cursor.getString(1);
         //POEM = cursor.getString(13);
         Log.e(poem, "================");
@@ -149,11 +162,74 @@ public class MainActivity extends Activity {
                 "Collect Delete",
                 Toast.LENGTH_SHORT).show();
     }
+    private void setmShareActionProvider(){
+        sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+//        sendIntent.putExtra(Intent.EXTRA_TEXT, poem);
+//        sendIntent.setType("text/plain");
+//        setShareIntent(sendIntent);
+        sendIntent.putExtra(Intent.EXTRA_STREAM, shot());
+        sendIntent.setType("image/jpeg");
+        setShareIntent(sendIntent);
+    }
+    private void setShareIntent(Intent shareIntent) {
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(shareIntent);
+        }
+    }
+    private Uri shot() {
+        View view = getWindow().getDecorView();
+        Display display = this.getWindowManager().getDefaultDisplay();
+        view.layout(0, 0, display.getWidth(), display.getHeight());
+        view.setDrawingCacheEnabled(true);//允许当前窗口保存缓存信息，这样getDrawingCache()方法才会返回一个Bitmap
+
+        Rect frame = new Rect();
+        this.getWindow().getDecorView().getWindowVisibleDisplayFrame(frame);
+        int statusBarHeight = frame.top;
+        // 获取屏幕长和高
+        int width = this.getWindowManager().getDefaultDisplay().getWidth();
+        int height = this.getWindowManager().getDefaultDisplay()
+                .getHeight();
+        TypedArray actionbarSizeTypedArray = this.obtainStyledAttributes(new int[]{
+                android.R.attr.actionBarSize});
+        int h = (int)actionbarSizeTypedArray.getDimension(0, 0);
+        Bitmap bmp = Bitmap.createBitmap(view.getDrawingCache(),0, statusBarHeight+h, width, height
+                - statusBarHeight-h);
+        File f = new File(Environment.getExternalStorageDirectory(),
+                "output_image.jpg");
+        FileOutputStream fOut = null;
+        try {
+            fOut = new FileOutputStream(f);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, fOut);
+        try {
+            assert fOut != null;
+            fOut.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            fOut.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Uri.fromFile(f);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
+
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        MenuItem menuItem = menu.findItem(R.id.like);
+        MenuItem item1 = menu.findItem(R.id.dislike);
+
+        // Fetch and store ShareActionProvider
+        mShareActionProvider = (ShareActionProvider) item.getActionProvider();
+
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -173,6 +249,10 @@ public class MainActivity extends Activity {
                 break;
             case R.id.dislike:
                 deleteAPoemFromCollect();
+                break;
+            case R.id.menu_item_share:
+                setmShareActionProvider();
+                startActivity(Intent.createChooser(sendIntent,"图片"));
                 break;
             default:
                 break;
